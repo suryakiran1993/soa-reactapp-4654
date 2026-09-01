b4a7bf56dc94ea210b6a787bdc6c7c358ff80964
J Surya Kiran <kiransurya93@kluniversity.in>
2026-09-01 13:09:12 +0530

    updated navbar

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/soa-reactapp-4654/'
+  base: '/soa-reactapp-4654/',
 })
